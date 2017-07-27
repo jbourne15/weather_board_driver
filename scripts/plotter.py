45a76b8ef6ae9ec75549b18eef28788f1c915004
damanfb <daman.bareiss@utah.edu>
2017-07-27 11:57:26 -0600

    switch to plotter instead of animations and fixed issue of multithreading and plotting.  Also added a launch file for all the plotting

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -10,67 +10,58 @@ from weather_board_driver.msg import wb_list
 ### Global variables.
 
 plt.ion()
-fig = plt.figure('humidity_plot', figsize=(10,10))
+fsize=(14,12)
+fig = plt.figure('humidity_plot', figsize=fsize)
 ax = fig.add_subplot(111, projection='3d')
-line = 0
+ax.grid(True)
+plt.xlabel(r'$x$ '+ r'$(m)$', fontsize=20)
+plt.ylabel(r'$y$ '+ r'$(m)$',fontsize=20)
 
-X = np.arange(5)
-Y = np.arange(5)
+
+X = np.linspace(0,8,5)
+Y = np.linspace(8,0,5)
 X, Y = np.meshgrid(X, Y)
+Z = None
+surf = None
         
-# Set up formatting for the movie files
-#Writer = animation.writers['ffmpeg']
-#writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+def getData_callback(data):
+    global Z
+    data = np.array(data.wb_list)
+    Z = np.array([ [data[0].humidity, data[5].humidity, data[10].humidity, data[15].humidity, data[20].humidity],
+                   [data[1].humidity, data[6].humidity, data[11].humidity, data[16].humidity, data[21].humidity],
+                   [data[2].humidity, data[7].humidity, data[12].humidity, data[17].humidity, data[22].humidity],
+                   [data[3].humidity, data[8].humidity, data[13].humidity, data[18].humidity, data[23].humidity],
+                   [data[4].humidity, data[9].humidity, data[14].humidity, data[19].humidity, data[24].humidity] ])
 
-###
-
-def plotCallback(data):
-    data = data.wb_list
-    global X, Y, ax, fig, Writer, writer, line
-
-    #DON'T TOUCH PAST HERE!!!------------------------------------------
-    #Except for a few lines for the lines.
-    ###        
-    ani = animation.FuncAnimation(fig, surface_updater, fargs=(data, line),
-                                       interval=50, blit=False)
-    ani.save('humidity.mp4', dpi=250)
+    #data = np.reshape(data,(5,5)).T
     ###
-
-def plotCallback2(data):
-    global X, Y, ax, fig
-    data = data.wb_list
-    Z = np.array([ [data[0].humidity, data[1].humidity, data[2].humidity, data[3].humidity, data[4].humidity],
-                   [data[5].humidity, data[6].humidity, data[7].humidity, data[8].humidity, data[9].humidity],
-                   [data[10].humidity, data[11].humidity, data[12].humidity, data[13].humidity, data[14].humidity],
-                   [data[15].humidity, data[16].humidity, data[17].humidity, data[18].humidity, data[19].humidity],
-                   [data[20].humidity, data[21].humidity, data[22].humidity, data[23].humidity, data[24].humidity] ])    
-    fig = plt.figure('humidity_plot', figsize=(10,10))
+    
+def surface_updater():
+    global X,Y,Z,fig,fsize,surf
     fig.clf()
-    
-    line = ax.plot_surface(X, Y, Z)#, cmap=cm.coolwarm)
-    fig.canvas.draw()
-    #time.sleep(.5)
-    
-    ###
-    
-def surface_updater(i, data, line):
-    
-    Z = np.array([ [data[0].humidity, data[1].humidity, data[2].humidity, data[3].humidity, data[4].humidity],
-                 [data[5].humidity, data[6].humidity, data[7].humidity, data[8].humidity, data[9].humidity],
-                 [data[10].humidity, data[11].humidity, data[12].humidity, data[13].humidity, data[14].humidity],
-                 [data[15].humidity, data[16].humidity, data[17].humidity, data[18].humidity, data[19].humidity],
-                 [data[20].humidity, data[21].humidity, data[22].humidity, data[23].humidity, data[24].humidity] ])
-         
-    ax.clear()
+    fig = plt.figure('humidity_plot', figsize=fsize)
+    ax = fig.add_subplot(111, projection='3d')
+    ax.grid(True)
+    plt.xlabel(r'$x$ '+ r'$(m)$', fontsize=20)
+    plt.ylabel(r'$y$ '+ r'$(m)$',fontsize=20)
 
-    line = ax.plot_surface(X, Y, Z)#, cmap=cm.coolwarm)
-    plt.show()
-    return line,
+    surf = ax.plot_surface(X, Y, Z, cmap='coolwarm')
+    ax.plot_wireframe(X, Y, Z,color='k')
+    fig.canvas.draw()
     
 #Init main
 if __name__ == '__main__':
-     
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("wb_list", wb_list, plotCallback2)
-    rospy.spin()
-
+    rospy.Subscriber("wb_list", wb_list, getData_callback)
+    rate = rospy.Rate(100)    
+    #import pdb; pdb.set_trace()
+    
+    while not rospy.is_shutdown():
+        if Z is not None:
+            surface_updater()
+        else:
+            print "No data recieved, start sensor_grid launch file"
+            time.sleep(1)
+            #rospy.loginfo_throttle(5, "No data recieved, start sensor_grid launch file")
+        rate.sleep()
+    #import pdb; pdb.set_trace()
